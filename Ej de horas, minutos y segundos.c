ac1ea702e929389cc6a79b034e84077bab16293a
Samuel <115562738+samuelrubio35@users.noreply.github.com>
2022-10-19 14:35:35 +0200

    Update Ej de horas, minutos y segundos.c

--- Ej de horas, minutos y segundos.c
+++ Ej de horas, minutos y segundos.c
@@ -10,22 +10,22 @@ int main()
     if (segundos ==59)
     {
         segundos = 0;
-        if (minutos == 59)
-        {
-            minutos = 0;
-            if (horas == 23)
-                horas = 0;
-            else
-                horas = horas + 1;
-        }
-        else
-             minutos = minutos + 1;
+    if (minutos == 59)
+     {
+        minutos = 0;
+    if (horas == 23)
+            horas = 0;
+     else
+     horas = horas + 1;
+     }
+     else
+     minutos = minutos + 1;
 
-    }
-    else
-        segundos += 1;
+     }
+     else
+     segundos += 1;
 
-    printf("%.2d:%.2d:%.2d", horas, minutos, segundos);
+     printf("%.2d:%.2d:%.2d", horas, minutos, segundos);
 
     return 0;
 }
