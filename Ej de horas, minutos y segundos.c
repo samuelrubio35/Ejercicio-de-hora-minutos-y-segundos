7faa7c184fc82c32ca7bfa0fae9dccf9e4e24a79
Samuel <115562738+samuelrubio35@users.noreply.github.com>
2022-10-19 14:32:39 +0200

    Add files via upload

---
+++ Ej de horas, minutos y segundos.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int horas, minutos, segundos;
+    printf("Introduzca la hora: ");
+    scanf("%d:%d:%d", &horas, &minutos, &segundos);
+
+    if (segundos ==59)
+    {
+        segundos = 0;
+        if (minutos == 59)
+        {
+            minutos = 0;
+            if (horas == 23)
+                horas = 0;
+            else
+                horas = horas + 1;
+        }
+        else
+             minutos = minutos + 1;
+
+    }
+    else
+        segundos += 1;
+
+    printf("%.2d:%.2d:%.2d", horas, minutos, segundos);
+
+    return 0;
+}
